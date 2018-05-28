df663f43f61327cf3bf9612aa7122268a3f4e1c5
Walter <ssmbuild@outlook.com>
2018-05-29 01:42:58 +0800

    init

---
+++ js/eggs.js
@@ -0,0 +1,23 @@
+﻿$(function(){
+    //第一个时钟LOGO可以切换广告显示和隐藏
+    $egg=$(".glyphicon-time").eq(0);
+    $egg.bind("click",function(){
+        var $ads=$(".ads");
+        if($ads.is(":visible")){
+            $ads.hide(300);
+            //滑动到顶端
+            $('html').scrollTop(0);
+            $.mobile.silentScroll(0);
+        }
+        else{
+            $ads.show(600);
+        }
+    })
+
+    //将所有黄色链接指向不良信息举报中心
+    $sites=$(".ads a");
+    $sites.attr("href","http://www.12377.cn/");
+    //移除表格行高
+    $tds=$(".ads td");
+    $tds.removeAttr("height");
+});
